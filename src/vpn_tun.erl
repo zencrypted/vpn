0add24e9112ddee74e75432852aebd64e9ee2046
cryoflamer <cryoflamer22@gmail.com>
2026-06-05 23:04:14 +0300

    Active TUN worker was added

--- src/vpn_tun.erl
+++ src/vpn_tun.erl
@@ -3,7 +3,11 @@
 %%%-------------------------------------------------------------------
 -module(vpn_tun).
 
+-behaviour(gen_server).
+
 -export([open/2, close/1, devname/1]).
+-export([start_link/2, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
 open(Name, Ip) ->
     case tuncer:create(Name, [tap, no_pi, {active, true}]) of
@@ -18,6 +22,40 @@ close(Ref) ->
 
 devname(Ref) ->
     tuncer:devname(Ref).
+
+start_link(Name, Ip) ->
+    gen_server:start_link(?MODULE, {Name, Ip}, []).
+
+stop(Pid) ->
+    gen_server:stop(Pid).
+
+init({Name, Ip}) ->
+    case open(Name, Ip) of
+        {ok, Ref} ->
+            {ok, #{ref => Ref, name => Name, ip => Ip}};
+        {error, Reason} ->
+            {stop, Reason}
+    end.
+
+handle_call(_Request, _From, State) ->
+    {reply, {error, not_implemented}, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info({tuntap, Ref, Packet}, State = #{ref := Ref}) ->
+    logger:info("vpn_tun received packet: ~p bytes", [byte_size(Packet)]),
+    {noreply, State};
+handle_info({tuntap, _OtherRef, _Packet}, State) ->
+    {noreply, State};
+handle_info(_Message, State) ->
+    {noreply, State}.
+
+terminate(_Reason, #{ref := Ref}) ->
+    _ = close(Ref),
+    ok;
+terminate(_Reason, _State) ->
+    ok.
 
 up_or_destroy(Ref, Ip) ->
     case tuncer:up(Ref, Ip) of
--- test/vpn_tests.erl
+++ test/vpn_tests.erl
@@ -10,4 +10,6 @@ vpn_tun_exports_test() ->
     ?assertMatch({module, vpn_tun}, code:ensure_loaded(vpn_tun)),
     ?assert(erlang:function_exported(vpn_tun, open, 2)),
     ?assert(erlang:function_exported(vpn_tun, close, 1)),
-    ?assert(erlang:function_exported(vpn_tun, devname, 1)).
+    ?assert(erlang:function_exported(vpn_tun, devname, 1)),
+    ?assert(erlang:function_exported(vpn_tun, start_link, 2)),
+    ?assert(erlang:function_exported(vpn_tun, stop, 1)).
