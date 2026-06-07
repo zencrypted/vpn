740762ad9bb6d98bdb3710283de4016e7ccd1844
Namdak Tonpa <maxim@synrc.com>
2026-06-08 00:03:42 +0300

    wip

--- src/vpn_app.erl
+++ src/vpn_app.erl
@@ -8,6 +8,12 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    case os:type() of
+        {unix, darwin} ->
+            application:set_env(procket, port_executable, "/usr/local/bin/procket");
+        _ ->
+            ok
+    end,
     vpn_sup:start_link().
 
 stop(_State) ->
